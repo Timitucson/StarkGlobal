895ee37dd8ab955ae349da94f4182a613fdb7752
Timitucson <lucasmartinf33@gmail.com>
2025-02-06 17:20:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Ejemplo de interactividad básica
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('header');
+    header.addEventListener('click', function () {
+        alert('¡Gracias por visitar StarkGlobal!');
+    });
+});
